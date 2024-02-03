aec0545b32c61cb3ae9e70c39659fd9195aa287e
anna <a.chervonuk@platim.ru>
2024-02-03 15:00:43 +0300

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import morgan from 'morgan'
 import Router from './routes'
+import swaggerUi from 'swagger-ui-express'
 
 const PORT = process.env.PORT || 8000
 
@@ -10,12 +11,17 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(express.static('public'))
 
-app.use(Router)
-app.get('ping', async (_req, res) => {
-    res.send({
-        message: 'pong'
+app.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+        swaggerOptions: {
+            url: '/swagger.json'
+        }
     })
-})
+)
+
+app.use(Router)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
